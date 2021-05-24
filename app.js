12ca91c47510cc4f16f9758dd4e8117cf6de725b
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-05-24 12:25:34 +0900

    初期情報設定

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const reviwes = [
   {
     id: 1,
@@ -6,4 +8,30 @@ const reviwes = [
     img: "./fruits/category_tn_192.jpeg",
     text: 山形県で多く取れます,
   },
+  {
+    id: 2,
+    fruits: Apple,
+    color: Red,
+    img: "./fruits/category_tn_193.jpeg",
+    text: 青森県で多く取れます,
+  },
+  {
+    id: 3,
+    fruits: Grape,
+    color: Purple,
+    img: "./fruits/category_tn_195.jpeg",
+    text: 山梨で多く取れます,
+  },
+  {
+    id: 4,
+    fruits: Banana,
+    color: Yellow,
+    img: "./fruits/category_tn_197.jpeg",
+    text: 朝食にぴったりです,
+  },
 ];
+
+const img = document.getElementById("fruits-img");
+const fruits = document.getElementById("fruits");
+const color = document.getElementById("color");
+const info = document.getElementById("info");
